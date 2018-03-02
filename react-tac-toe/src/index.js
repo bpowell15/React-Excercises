2696b59dbde22840d573daad5600334875e7bd22
bpowell15 <bpaulpowell@gmail.com>
2018-03-02 14:07:34 -0500

    Remove commented out code

--- react-tac-toe/src/index.js
+++ react-tac-toe/src/index.js
@@ -3,15 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
-// class Square extends React.Component {
-//   render() {
-//     return (
-//       <button className="square" onClick={()=> this.props.onClick()}>
-//         {this.props.value}
-//       </button>
-//     );
-//   }
-// }
+
 
 function Square(props) {
   return (
@@ -23,27 +15,6 @@ function Square(props) {
 
 
 class Board extends React.Component {
-  // constructor(props){
-  //   super(props);
-  //   this.state = {
-  //     squares: Array(9).fill(null),
-  //     xIsNext: true
-  //   };
-  //
-  // }
-
-  // handleClick(i){
-  //   const squares = this.state.squares.slice();
-  //   if (calculateWinner(squares) || squares[i]) {
-  //     return;
-  //   }
-  //   squares[i] = this.state.xIsNext ? 'X' : 'O';
-  //   this.setState({
-  //     squares: squares,
-  //     xIsNext: !this.state.xIsNext
-  //   });
-  // }
-
   renderSquare(i) {
     return (
       <Square
@@ -54,18 +25,6 @@ class Board extends React.Component {
   }
 
   render() {
-    // const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    // const winner =
-    // calculateWinner(this.state.squares);
-    // let status;
-    // if (winner) {
-    //   status = 'Winner: ' + winner;
-    // } else if (!this.state.squares.includes(null)) {
-    //   status = 'Tie!';
-    // } else {
-    //   status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    // }
-
     return (
       <div>
         <div className="board-row">
